49d9ee3ca757482ad4df8db3d690e5376544a7f8
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-01 20:13:54 +0300

    preliminary implementation of traceroute command

---
+++ cmd/traceroute.go
@@ -0,0 +1,52 @@
+// Copyright © 2016 Nagy Károly Gábriel <karasz@jpi.io>
+// This file, part of {{ .appName }}, is free and unencumbered software
+// released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+//
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/karasz/genet/genetlib"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// tracerouteCmd represents the traceroute command
+var tracerouteCmd = &cobra.Command{
+	Use:          "traceroute",
+	Short:        "Trace route to host",
+	SilenceUsage: true,
+	Long:         `Traceroute emulates the traceroute command from linux`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("you must provide a host to ping as the first argument")
+		}
+		return nil
+	},
+	Run: doTraceroute,
+}
+
+func init() {
+	versionCmd.SetUsageTemplate("Usage: \n\tgenet traceroute HOST\n\n")
+	RootCmd.AddCommand(tracerouteCmd)
+
+}
+
+func doTraceroute(cmd *cobra.Command, args []string) {
+	addr := args[0]
+	if addr == "" {
+		fmt.Fprintf(os.Stderr, "An empty address was provided. You must provide an address as the first argument.\n")
+		os.Exit(1)
+	}
+
+	res, err := genetlib.Traceroute(addr)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+}
---
+++ genetlib/tracecoute.go
@@ -0,0 +1,109 @@
+package genetlib
+
+import (
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+type TracerouteHop struct {
+	Address net.IP
+	Host    []string
+	RTT     time.Duration
+}
+
+type TracerouteResult struct {
+	Address net.IP
+	Hops    []TracerouteHop
+}
+
+func Traceroute(host string) (TracerouteResult, error) {
+	var result TracerouteResult
+
+	var dst net.IPAddr
+
+	dst.IP = net.ParseIP(host)
+	if dst.IP == nil {
+		ips, err := net.LookupIP(host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, ip := range ips {
+			if ip.To4() != nil {
+				dst.IP = ip
+				result.Address = dst.IP
+				break
+			}
+		}
+	} else {
+		result.Address = dst.IP
+	}
+
+	c, err := net.ListenPacket("ip4:1", "0.0.0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+	p := ipv4.NewPacketConn(c)
+
+	if err := p.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
+		log.Fatal(err)
+	}
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID:   os.Getpid() & 0xffff,
+			Data: []byte("HELLO-R-U-THERE"),
+		},
+	}
+
+	rb := make([]byte, 1500)
+	for i := 1; i <= 64; i++ {
+		var hop TracerouteHop
+		wm.Body.(*icmp.Echo).Seq = i
+		wb, err := wm.Marshal(nil)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := p.SetTTL(i); err != nil {
+			log.Fatal(err)
+		}
+
+		begin := time.Now()
+		if _, err := p.WriteTo(wb, nil, &dst); err != nil {
+			log.Fatal(err)
+		}
+		if err := p.SetReadDeadline(time.Now().Add(3 * time.Second)); err != nil {
+			log.Fatal(err)
+		}
+		n, _, peer, err := p.ReadFrom(rb)
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				continue
+			}
+			log.Fatal(err)
+		}
+		rm, err := icmp.ParseMessage(1, rb[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+		rtt := time.Since(begin)
+
+		if rm.Type == ipv4.ICMPTypeTimeExceeded || rm.Type == ipv4.ICMPTypeEchoReply {
+			names, _ := net.LookupAddr(peer.String())
+			hop.Address = net.ParseIP(peer.String())
+			hop.Host = names
+			hop.RTT = rtt
+			result.Hops = append(result.Hops, hop)
+		}
+		if rm.Type == ipv4.ICMPTypeEchoReply {
+			break
+		}
+	}
+
+	return result, nil
+}
