d18d401523e4f33bda9d250333a8a55dd4b08449
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-02 13:48:44 +0300

    genet: refined traceroute to process only packages that are addressed to it

--- genetlib/tracecoute.go
+++ genetlib/tracecoute.go
@@ -1,6 +1,7 @@
 package genetlib
 
 import (
+	//	"fmt"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"log"
@@ -24,7 +25,7 @@ func Traceroute(host string) (TracerouteResult, error) {
 	var result TracerouteResult
 
 	var dst net.IPAddr
-
+	outIP := GetOutboundIP()
 	dst.IP = net.ParseIP(host)
 	if dst.IP == nil {
 		ips, err := net.LookupIP(host)
@@ -69,39 +70,60 @@ func Traceroute(host string) (TracerouteResult, error) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		if err := p.SetTTL(i); err != nil {
 			log.Fatal(err)
 		}
 
 		begin := time.Now()
+
 		if _, err := p.WriteTo(wb, nil, &dst); err != nil {
 			log.Fatal(err)
 		}
+
 		if err := p.SetReadDeadline(time.Now().Add(3 * time.Second)); err != nil {
 			log.Fatal(err)
 		}
-		n, _, peer, err := p.ReadFrom(rb)
+
+		n, cm, peer, err := p.ReadFrom(rb)
+
 		if err != nil {
 			if err, ok := err.(net.Error); ok && err.Timeout() {
 				continue
 			}
 			log.Fatal(err)
 		}
+
 		rm, err := icmp.ParseMessage(1, rb[:n])
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		rtt := time.Since(begin)
 
-		if rm.Type == ipv4.ICMPTypeTimeExceeded || rm.Type == ipv4.ICMPTypeEchoReply {
-			names, _ := net.LookupAddr(peer.String())
-			hop.Address = net.ParseIP(peer.String())
-			hop.Host = names
-			hop.RTT = rtt
-			result.Hops = append(result.Hops, hop)
-		}
 		if rm.Type == ipv4.ICMPTypeEchoReply {
-			break
+			if rm.Body.(*icmp.Echo).Seq == i {
+				rtt := time.Since(begin)
+
+				names, _ := net.LookupAddr(peer.String())
+				hop.Address = net.ParseIP(peer.String())
+				hop.Host = names
+				hop.RTT = rtt
+				result.Hops = append(result.Hops, hop)
+				break
+			}
+		} else {
+			if rm.Type == ipv4.ICMPTypeTimeExceeded {
+				if cm.Dst.String() == outIP {
+					rtt := time.Since(begin)
+
+					names, _ := net.LookupAddr(peer.String())
+					hop.Address = net.ParseIP(peer.String())
+					hop.Host = names
+					hop.RTT = rtt
+					result.Hops = append(result.Hops, hop)
+
+				}
+			}
 		}
 	}
 
