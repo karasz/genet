e08f09febd48b487b41f78d4d77868f3d673e302
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-08 21:17:33 +0300

    genetlib: introduced disk statistics

--- genetlib/stats.go
+++ genetlib/stats.go
@@ -37,11 +37,31 @@ type procstats struct {
 	Softirq       string
 }
 
+type diskstats struct {
+	Major             int64
+	Minor             int64
+	Name              string
+	Reads_ok          int64
+	Reads_m           int64
+	Sectors_read      int64
+	Time_reading      time.Duration
+	Writes_ok         int64
+	Writes_m          int64
+	Sectors_written   int64
+	Time_writting     time.Duration
+	IOs_current       int64
+	Time_IOs          time.Duration
+	Weighted_time_IOs time.Duration
+}
+
 func (p procstats) String() string {
 	return fmt.Sprintf("Cpu: %v Ctxt: %v Btime: %v Processes: %v Procs_running: %v Procs_blocked: %v SoftIRQs: %v", p.Cpu, p.Ctxt, p.Btime, p.Processes, p.Procs_running, p.Procs_blocked, p.Softirq)
 
 }
 
+func (d diskstats) String() string {
+	return fmt.Sprintf("Name: %v Major: %v Minor: %v reads completed successfully: %v reads merged: %v sectors read: %v time spent reading: %v writes completed: %v writes merged: %v sectors written: %v time spent writing: %v I/Os currently in progress: %v time spent doing I/Os: %v weighted time spent doing I/Os: %v", d.Name, d.Major, d.Minor, d.Reads_ok, d.Reads_m, d.Sectors_read, d.Time_reading, d.Writes_ok, d.Writes_m, d.Sectors_written, d.Time_writting, d.IOs_current, d.Time_IOs, d.Weighted_time_IOs)
+}
 func stringtoint64(s string) int64 {
 	result, _ := strconv.ParseInt(s, 10, 64)
 	return result
@@ -111,6 +131,35 @@ func GetStatsNet() (string, error) {
 func GetStatsAll() (string, error) {
 	return "Not implemented", nil
 }
-func GetStatsDisk() (string, error) {
-	return "Not implemented", nil
+func GetStatsDisk() ([]diskstats, error) {
+	var result []diskstats
+
+	lines, err := ReadLines("/proc/diskstats")
+	if err != nil {
+		return result, err
+	}
+
+	for _, line := range lines {
+		// filter out loop devices and ram
+		if !strings.HasPrefix(strings.Fields(line)[2], "ram") && !strings.HasPrefix(strings.Fields(line)[2], "loop") {
+			var res diskstats
+			res.Major = stringtoint64(strings.Fields(line)[0])
+			res.Minor = stringtoint64(strings.Fields(line)[1])
+			res.Name = strings.Fields(line)[2]
+			res.Reads_ok = stringtoint64(strings.Fields(line)[3])
+			res.Reads_m = stringtoint64(strings.Fields(line)[4])
+			res.Sectors_read = stringtoint64(strings.Fields(line)[5])
+			res.Time_reading = time.Duration(stringtoint64(strings.Fields(line)[6]) * 1000000)
+			res.Writes_ok = stringtoint64(strings.Fields(line)[7])
+			res.Writes_m = stringtoint64(strings.Fields(line)[8])
+			res.Sectors_written = stringtoint64(strings.Fields(line)[9])
+			res.Time_writting = time.Duration(stringtoint64(strings.Fields(line)[10]) * 1000000)
+			res.IOs_current = stringtoint64(strings.Fields(line)[11])
+			res.Time_IOs = time.Duration(stringtoint64(strings.Fields(line)[12]) * 1000000)
+			res.Weighted_time_IOs = time.Duration(stringtoint64(strings.Fields(line)[13]) * 100000)
+			result = append(result, res)
+
+		}
+	}
+	return result, nil
 }
